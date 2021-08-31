fac75f2c36e5f9ed7801a8d7d655b32b4b44048d
PvPatel-1001 <pvpatel150398@gmail.com>
2021-08-31 15:50:20 -0400

    added division function

--- code/__init__.py
+++ code/__init__.py
@@ -11,8 +11,8 @@ except:
 	msg = "Please provide your details in the \"User_parameters\\user_info\" file for more personalised messages.\nIf you don't have the parameters file contact developers.\nYou can follow the instructions in the readme.md to fill the parameters file"
 
 def divide(a, b):
-	if isnumeric(a) and isnumeric(b):
-		return (-1, msg, "please provide valid numeric inputs")
-	if b == 0:
-		return (-1, msg, "Can not divide by zero")
-	return (a/b, msg, "")
+	if a.isnumeric() and b.isnumeric():
+		if float(b) != 0.0:
+			return int(a)/int(b), msg, ""
+		else:
+			return -1, msg, "invalid input"
