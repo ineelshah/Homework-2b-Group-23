d4544e0e364f6d664a6d8301264350ea4b223d60
Neel Shah <neelshah.2798@gmail.com>
2021-08-31 20:38:22 -0400

    Added Greetings Tests

---
+++ test/test_greetings.py
@@ -0,0 +1,7 @@
+from src import greetings
+
+def greetings_util():
+	return greetings.greet()
+
+def test_capitalize_string():
+	assert greetings_util() == "Please provide your details in the \"User_parameters\\user_info\" file for more personalised messages.\nIf you don't have the parameters file contact developers.\nYou can follow the instructions in the readme.md to fill the parameters file"
